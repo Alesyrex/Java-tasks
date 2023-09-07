235f3a856e7b5cb20052ef02ac160638f3b0fcb4
Alesyrex <Alesyrex@gmail.com>
2023-09-07 18:55:44 +0300

    relize task4 to multithread

--- src/kap/newbie/multithreading/test/task4synchronized/SynchMain.java
+++ src/kap/newbie/multithreading/test/task4synchronized/SynchMain.java
@@ -7,12 +7,49 @@ import kap.newbie.multithreading.test.task4synchronized.service.CarService;
  * @author Alexandr Korovkin
  */
 public class SynchMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Car[] arrayCars = createCars();
 
         CarService service = new CarService(arrayCars);
 
+        Thread.currentThread().setName("one");
+        Thread two = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                service.addCar
+                        (new Car("RR-123-RR", 2001, "blue", false));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "two");
+        two.start();
+
+        Thread three = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                service.deleteCar("RR-555-RR", 2018);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "three");
+        three.start();
+
+        Thread four = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                service.printCars();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "four");
+
+        four.start();
+
         System.out.println(service.search("RR-666-RR", 2012));
+
+        two.join();
+        three.join();
+        four.join();
     }
 
     private static Car[] createCars() {
@@ -24,7 +61,7 @@ public class SynchMain {
                 new Car("RR-555-RR", 2018, "silver", true),
                 new Car("RR-555-RR", 2005, "yellow", true),
                 new Car("RR-555-RR", 2012, "black",  true),
-                new Car("RR-666-RR", 2018, "yellow", true),
+                new Car("RR-666-RR", 2012, "yellow", true),
                 new Car("RR-777-RR", 2018, "white",  true),
                 new Car("RR-888-RR", 2018, "yellow", true),
                 new Car("RR-999-RR", 2018, "silver", true),
--- src/kap/newbie/multithreading/test/task4synchronized/service/CarService.java
+++ src/kap/newbie/multithreading/test/task4synchronized/service/CarService.java
@@ -22,33 +22,53 @@ public class CarService {
         this.cars = Arrays.asList(cars);
     }
 
-    public synchronized void addCar(Car car){
+    public synchronized void addCar(Car car) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " using method add");
+        Thread.sleep(2000);
         cars.add(car);
     }
 
-    public synchronized void deleteCar(String number, int year){
+    public synchronized void deleteCar(String number, int year) throws InterruptedException {
         deleteCar(new CarIdentifier(number, year));
     }
 
-    public synchronized void deleteCar(CarIdentifier identifier){
+    public synchronized void deleteCar(CarIdentifier identifier) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " using method delete");
+        Thread.sleep(2000);
         cars = cars.stream()
                 .filter(c -> c.getIdentifier() != identifier)
                 .collect(Collectors.toList());
     }
 
-    public synchronized Car search(String number, int year){
+    public synchronized Car search(String number, int year) throws InterruptedException {
         return search(new CarIdentifier(number, year));
     }
 
-    public synchronized Car search(Car car){
+    public synchronized Car search(Car car) throws InterruptedException {
         CarIdentifier id = car.getIdentifier();
         return search(id);
     }
 
-    public synchronized Car search(CarIdentifier id){
+    public synchronized Car search(CarIdentifier id) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " using method search");
+        Thread.sleep(2000);
         return cars.stream()
                 .filter(c -> c.getIdentifier().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Car not found"));
+    }
+
+    public void changeColor(String number, int year, String color) throws InterruptedException {
+        search(number, year).setColor(color);
+    }
+
+    public void changeTechnicalInspection(String number, int year, boolean inspection) throws InterruptedException {
+        search(number,year).setActualTechnicalInspection(inspection);
+    }
+
+    public synchronized void printCars() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " using method print");
+        Thread.sleep(2000);
+        cars.forEach(System.out::println);
     }
 }
